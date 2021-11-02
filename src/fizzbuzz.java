37f3cb2f929b6a6a214dca259358705a30ae952e
as26uk <ajz@live.co.uk>
2021-11-02 08:49:58 +0000

    code cleaned up

--- src/fizzbuzz.java
+++ src/fizzbuzz.java
@@ -2,29 +2,18 @@ public class fizzbuzz {
     public static void main(String[] args) {
        int n = 30;
        String a = " ";
-       System.out.println(" ");
+       System.out.println(a);
 
        for (int i = 1;  i < n+1; i++) {
+           if (i % 15 == 0) a = "FIZZBUZZ";
+           else if (i % 5 == 0) a = "Buzz";
+           else if (i % 3 == 0) a = "Fizz";
+           else a = " ";
 
-           if (i % 15 == 0) {
-               a = "FizzBuzz";
-
-           } else if (i % 5 == 0) {
-               a = "Buzz";
-
-           } else if (i % 3 == 0) {
-               a = "Fizz";
-
-           } else {
-               a = " ";
-
-           }if (a == " ") {
-               System.out.println(i);
-
-           } else {
-               System.out.println(a);
+           if (a.equals(" ")) System.out.println(i);
+           else System.out.println(a);
            }
        }
     }
-}
+
 
